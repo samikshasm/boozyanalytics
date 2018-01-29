0af24c3371fd7a024731c5433f8160398730d520
Alex <lambrechtae@slu.edu>
2018-01-28 18:00:46 -0600

    adding new version

--- app/app.js
+++ app/app.js
@@ -1,14 +1,11 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('new_test_project', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'new_test_project.home'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/home'});
 }]);
---
+++ app/home/home.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('new_test_project.home', ['ngRoute', 'firebase'])
+
+.config(['$routeProvider', function($routeProvider){
+  $routeProvider.when('/home', {
+    templateUrl: 'home/home.html',
+    controller: 'HomeCtrl'
+  });
+}])
+.controller('HomeCtrl', ['$scope', '$firebaseAuth', function($scope, $firebaseAuth){
+  $scope.signIn = function(){
+    var username = $scope.user.email;
+    var password = $scope.user.password;
+    var auth = $firebaseAuth();
+
+    auth.$signInWithEmailAndPassword(username, password).then(function(){
+      console.log("User Login Successful");
+      $scope.errMsg = false;
+    }).catch(function(error){
+      $scope.errorMessage = error.message;
+      $scope.errMsg = true;
+    });
+  }
+
+}])
