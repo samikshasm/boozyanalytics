d74c2127770e1d9d89243648f27a85cbaffa293a
Alex <lambrechtae@slu.edu>
2018-02-21 12:56:41 -0600

    adding favicon

--- app/scripts/controllers/users.js
+++ app/scripts/controllers/users.js
@@ -4,15 +4,68 @@ angular.module('angularAppApp.users', ['ngRoute','firebase'])
 
 
 .controller('UserCtrl', ['$scope', 'CommonProp', '$firebaseArray', '$firebaseObject', function($scope, CommonProp, $firebaseArray, $firebaseObject){
-  
-  var admin = require('firebase-admin');
 
-  var serviceAccount = require('/slu-capstone-f622b-firebase-adminsdk-xt82h-47a67b3abd.json');
+  var ref = firebase.database().ref();
+  var dataRef = $firebaseArray(ref);
+  $scope.articles = [];
+  $scope.names = [];
+  var uid ="";
+  var nightCount = "";
+  var date = "";
+  var time = "";
+  var size = "";
+  var type = "";
+  var where = "";
+  var who = "";
+  var dateCounter = 0;
+  var whoCounter = 0;
+  var whereCounter = 0;
 
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://slu-capstone-f622b.firebaseio.com'
-  });
+  dataRef.$loaded()
+    .then(function(){
+        angular.forEach(dataRef, function(value) {
+          angular.forEach(value, function(value, id){
+            var substr = id.substr(0,3);
+            if(substr == "UID"){
+              uid = id.substr(5,id.length);
+              //$scope.articles.push({"key":uid})
+
+            }
+            angular.forEach(value, function(value, id){
+              var idStr = ""+id;
+              var substr = idStr.substr(0,11);
+              if(substr == "Night Count"){
+                dateCounter++;
+                nightCount = id.substr(13,idStr.length);
+                //$scope.articles.push({"value":nightCount})
+
+              }
+              angular.forEach(value, function(value, id){
+                angular.forEach(value, function(value,id){
+                  var idStr = ""+id;
+                  var substr = idStr.substr(0,4);
+                  if(substr == "Date"){
+                    date = idStr.substr(5,idStr.length);
+                    //$scope.articles.push({"date":date})
+                    $scope.articles.push({"key":uid,"value":dateCounter,"date":date})
+                    dateCounter=0;
+                  }
+                })
+                })
+
+              })
+            })
+            //console.log(id+":"+value);
+          })
+        /*  $scope.articles.push({
+            "key": uid,
+            "value": nightCount,
+            "date": date*/
+            //"time": time
+          //})
+
+    });
+
 
 
 }])
