21aa3dc881dc516210633f6e6924bfbd0bfdf08c
samikshasm <mailarpwarss@slu.edu>
2018-02-27 08:51:24 -0600

    trying to fix Modal + deleteUser code

--- app/scripts/controllers/welcome.js
+++ app/scripts/controllers/welcome.js
@@ -24,6 +24,91 @@ angular.module('angularAppApp.welcome', ['ngRoute'])
     CommonProp.logoutUser();
   }
 
+	var config = {
+    apiKey: "AIzaSyB0QGhWIaE6wjxO9Y_AJCLXm8h3hJjj34Y",
+    authDomain: "slu-capstone-f622b.firebaseapp.com",
+    databaseURL: "https://slu-capstone-f622b.firebaseio.com",
+    projectId: "slu-capstone-f622b",
+    storageBucket: "slu-capstone-f622b.appspot.com",
+    messagingSenderId: "931206697482"
+  };
+
+	$scope.deleteUser = function(name){
+	/*	console.log("delete user function");
+		var deleteApp = firebase.initializeApp(config,"Delete current User");
+		var username = $scope.user.email;
+		var usernameEmail = username+"@gmail.com";
+		var password = "hello123";
+
+		console.log(usernameEmail);
+		console.log(username); */
+
+		console.log(name);
+
+		/*if(usernameEmail && password) {
+			deleteApp.auth().signInWithEmailAndPassword(usernameEmail, password).then(function(){
+				var user = deleteApp.auth().currentUser;
+				user.delete().then(function() {
+					console.log("User Successfully Deleted");
+					var user = deleteApp.auth().currentUser;
+					if(!user){
+						deleteApp.delete();
+					}
+				}).catch(function(error) {
+					$scope.errMsg = true;
+					$scope.errorMessage = error.message;
+				});
+			}).catch(function(error){
+				$scope.errMsg = true;
+				$scope.errorMessage = error.message;
+			});
+		}
+
+		var ref = firebase.database().ref();
+		var dataRef = $firebaseArray(ref);
+		var controlGroupNames = [];
+		var experimentalGroupNames = [];
+
+		dataRef.$loaded()
+			.then(function(){
+					angular.forEach(dataRef, function(value) {
+						angular.forEach(value, function(value, id){
+							 if(id == "Control Group"){
+								 angular.forEach(value, function(value, id){
+									 controlGroupNames.push(id);
+								 })
+							 }else{
+								 angular.forEach(value, function(value, id){
+									 experimentalGroupNames.push(id);
+								 })
+							 }
+							})
+						})
+
+						for (var i=0; i<controlGroupNames.length;i++) {
+							if (controlGroupNames[i] == username) {
+								$scope.typeOfGroup = "control";
+							}
+						}
+						for (var i=0; i<experimentalGroupNames.length;i++) {
+							if (experimentalGroupNames[i] == username) {
+								$scope.typeOfGroup = "experimental";
+							}
+						}
+
+						if ($scope.typeOfGroup == "control") {
+							var ref = firebase.database().ref("Users/Control Group/"+username);
+							ref.remove();
+						}else{
+							var ref = firebase.database().ref("Users/Experimental Group/"+username);
+							ref.remove();
+						}
+				});*/
+	}
+
+
+
+
   var ref = firebase.database().ref();
   var dataRef = $firebaseArray(ref);
   $scope.articles = [];
