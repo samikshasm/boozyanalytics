0314c31918c02a3f43120cea59382edae208ca83
samikshasm <mailarpwarss@slu.edu>
2018-03-24 15:18:29 -0500

    Pie chart + google maps

--- app/scripts/controllers/participantInfo.js
+++ app/scripts/controllers/participantInfo.js
@@ -6,12 +6,30 @@ var userModule = angular.module('angularAppApp.participantInfo',['ngRoute','fire
   $scope.currentParticpant = SetCurrentUser.getCurrentUser()
   $scope.controlList = SetCurrentUser.getControlList();
   $scope.experimentalList = SetCurrentUser.getExperimentalList();
+  $scope.totalCalConsumed = 0;
+  $scope.totalLitersConsumed = 0;
 
   var ref = firebase.database().ref();
   var dataRef = $firebaseArray(ref);
   var group = "";
   var participantName = "";
   var nightCounter = 0;
+  $scope.sizeList = [];
+  $scope.typeList = [];
+  $scope.totalDrinks = 0;
+  var whereList = [];
+  var whoList = [];
+  $scope.locationCounter = 0;
+  $scope.lattitudeList = [];
+  $scope.longitudeList = [];
+  $scope.percentWine = 0.0;
+  $scope.percentLiquor = 0.0;
+  $scope.percentBeer = 0.0;
+  var wineCounter = 0;
+  var beerCounter = 0;
+  var liquorCounter = 0;
+
+
 
   for(var i=0;i<$scope.controlList.length;i++){
     if($scope.currentParticpant == $scope.controlList[i]){
@@ -30,7 +48,6 @@ var userModule = angular.module('angularAppApp.participantInfo',['ngRoute','fire
   function queryDatabase(){
     var userRef = firebase.database().ref('Users/');
     userRef.on('value', function(snapshot){
-      var sizeList = [];
       dataRef.$loaded()
         .then(function(){
             angular.forEach(dataRef, function(value, id) {
@@ -42,13 +59,11 @@ var userModule = angular.module('angularAppApp.participantInfo',['ngRoute','fire
                     if(substr == "UID"){
                       participantName = id.substr(5,id.length);
                       if(participantName == $scope.currentParticpant) {
-                        console.log("yes");
                         angular.forEach(value, function(value, id){
                           var idStr = ""+id;
                           var substr = idStr.substr(0,11);
                           if(substr == "Night Count"){
                             nightCounter++;
-                            console.log(nightCounter);
                           }
                         })
 
@@ -57,12 +72,38 @@ var userModule = angular.module('angularAppApp.participantInfo',['ngRoute','fire
                             if(id == "Answers"){
                               angular.forEach(value, function(value,id){
                                 angular.forEach(value, function(value,id){
-
+                                  if(id == "Size"){
+                                    angular.forEach(value,function(value,id){
+                                      $scope.sizeList.push(value);
+                                      $scope.totalDrinks++;
+                                    })
+                                  }
+                                  if(id == "Type"){
+                                    angular.forEach(value,function(value,id){
+                                      $scope.typeList.push(value);
+                                    })
+                                  }
+                                  if(id == "Where"){
+                                    angular.forEach(value,function(value,id){
+                                      whereList.push(value);
+                                    })
+                                  }
+                                  if(id == "Who"){
+                                    angular.forEach(value,function(value,id){
+                                      whoList.push(value);
+                                    })
+                                  }
                                 })
                               })
                             }
                             if(id == "Location"){
-
+                              angular.forEach(value,function(value,id){
+                                var tempList = []
+                                $scope.locationCounter++;
+                                tempList = value.split("&");
+                                $scope.lattitudeList.push(tempList[0]);
+                                $scope.longitudeList.push(tempList[1]);
+                              })
                             }
                             if(id == "MorningAnswers"){
 
@@ -72,36 +113,155 @@ var userModule = angular.module('angularAppApp.participantInfo',['ngRoute','fire
                       }
 
                     }
-                  /*  angular.forEach(value, function(value, id){
-                      var idStr = ""+id;
-                      var substr = idStr.substr(0,11);
-                      if(substr == "Night Count"){
-                        dateCounter++;
-                        //nightCount = id.substr(13,idStr.length);
-                        //$scope.articles.push({"value":nightCount})
-
-                      }
-                      angular.forEach(value, function(value, id){
-                        angular.forEach(value, function(value,id){
-                          var idStr = ""+id;
-                          var substr = idStr.substr(0,4);
-                          if(substr == "Date"){
-                            date = idStr.substr(5,idStr.length);
-                            //$scope.articles.push({"date":date})
-                            uidList.push(uid);
-                            //$scope.articles.push({"key":uid,"value":dateCounter,"date":date});
-                            dateList.push(date);
-                            dateCounter=0;
-                          }
-                        })
-                        })
-
-                      })*/
                     }
                   })
               })
+              var calorieWineShot = 100; //i made this up
+              var calorieWineEight = 188;
+              var calorieWineSixteen = 377;
+              var calorieWineTwentyFour = 565;
+
+              var calorieBeerShot = 58; //i don't actually know
+              var calorieBeerEight = 98;
+              var calorieBeerSixteen = 196;
+              var calorieBeerTwentyFour = 294;
+
+              var calorieLiquorShot = 100; //i made this up
+              var calorieLiquorEight = 188;
+              var calorieLiquorSixteen = 377;
+              var calorieLiquorTwentyFour = 565;
+
+              var totalOuncesConsumed = 0;
+              var tempSize = 0;
+              var tempType = 0;
+
+              for(var i=0;i<$scope.sizeList.length;i++){
+                tempType = $scope.typeList[i];
+                tempSize = $scope.sizeList[i];
+
+                if (!$scope.sizeList[i]=="Shot") {
+                  var sizeOfDrink = Integer.parseInt($scope.sizeList[i]);
+                  totalOuncesConsumed = totalOuncesConsumed + sizeOfDrink;
+                }
+                else {
+                  totalOuncesConsumed = totalOuncesConsumed + 1;
+                }
+
+                if(tempType == "wine"){
+                  wineCounter++;
+                  if(tempSize == "Shot"){
+                    $scope.totalCalConsumed = $scope.totalCalConsumed+calorieWineShot;
+                  }
+                  else if (tempSize == "8") {
+                    $scope.totalCalConsumed = $scope.totalCalConsumed+calorieWineEight;
+                  }
+                  else if (tempSize == "16") {
+                    $scope.totalCalConsumed = $scope.totalCalConsumed+calorieWineSixteen;
+                  }
+                  else if (tempSize == "24") {
+                    $scope.totalCalConsumed = $scope.totalCalConsumed+calorieWineTwentyFour;
+                  }
+                }
+
+                else if(tempType == "liquor"){
+                  liquorCounter++;
+                  if(tempSize == "Shot"){
+                    $scope.totalCalConsumed = $scope.totalCalConsumed+calorieLiquorShot;
+                  }
+                  else if (tempSize == "8") {
+                    $scope.totalCalConsumed = $scope.totalCalConsumed+calorieLiquorEight;
+                  }
+                  else if (tempSize == "16") {
+                    $scope.totalCalConsumed = $scope.totalCalConsumed+calorieLiquorSixteen;
+                  }
+                  else if (tempSize == "24") {
+                    $scope.totalCalConsumed = $scope.totalCalConsumed+calorieLiquorTwentyFour;
+                  }
+                }
+
+                else if(tempType == "beer"){
+                  beerCounter++;
+                  if(tempSize == "Shot"){
+                    $scope.totalCalConsumed = $scope.totalCalConsumed+calorieBeerShot;
+                  }
+                  else if (tempSize == "8") {
+                    $scope.totalCalConsumed = $scope.totalCalConsumed+calorieBeerEight;
+                  }
+                  else if (tempSize == "16") {
+                    $scope.totalCalConsumed = $scope.totalCalConsumed+calorieBeerSixteen;
+                  }
+                  else if (tempSize == "24") {
+                    $scope.totalCalConsumed = $scope.totalCalConsumed+calorieBeerTwentyFour;
+                  }
+                }
+              }
+
+              $scope.totalLitersConsumed = (totalOuncesConsumed * 0.03);
+
+              $scope.percentWine = (wineCounter/$scope.totalDrinks) * 100;
+              $scope.percentBeer = (beerCounter/$scope.totalDrinks) *100;
+              $scope.percentLiquor = (liquorCounter/$scope.totalDrinks) *100;
+
+
+              var ctxP = document.getElementById("myChart").getContext('2d');
+              var myPieChart = new Chart(ctxP, {
+              type: 'pie',
+              data: {
+                  labels: ["Wine", "Beer", "Liquor"],
+                  datasets: [
+                      {
+                          data: [$scope.percentWine,$scope.percentBeer,$scope.percentLiquor],
+                          backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C"],
+                          hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870"]
+                      }
+                  ]
+              },
+              options: {
+                  responsive: true
+              }
+              });
+
+              var var_location = [];
+              for(var i =0;i<$scope.lattitudeList.length;i++){
+                var_location.push(new google.maps.LatLng(parseFloat($scope.lattitudeList[i]), parseFloat($scope.longitudeList[i])));
+              }
+              /*var var_map = new google.maps.Map(document.getElementById("map-container-5"),
+                  var_mapoptions);
+
+                  var var_mapoptions = {
+                      center: var_location[0],
+                      zoom: 14
+                  };
+
+                  var var_marker = new google.maps.Marker({
+                      position: var_location[0],
+                      map: var_map,
+                      title: "New York"
+                  }); */
+
+            var var_markers = [];
+
+
+            for(var i=0; i<$scope.locationCounter;i++){
+              var_markers.push(new google.maps.Marker({
+                position: var_location[i],
+                map: var_map,
+                title: ""+i
+              }));
+            }
+
+            var var_map = new google.maps.Map(document.getElementById("map-container-5"),
+                var_mapoptions);
+
+            var var_mapoptions = {
+                center: var_location[i],
+                zoom: 14
+             };
+
+            //  google.maps.event.addDomListener(window, 'load', regular_map);
 
         });
     })
   }
+
 }])
